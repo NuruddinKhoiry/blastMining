d0ec923b90d9365c2aa40e40dca2fb81547c99e6
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-06-21 11:49:47 +0700

    Update blastMining.py

--- blastMining/blastMining.py
+++ blastMining/blastMining.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-blastMining 0.1.0
+blastMining 0.1.1
 
 BLAST outfmt 6 only:
 ("qseqid","sseqid","pident","length","mismatch","gapopen","evalue","bitscore","staxid")  
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import argparse
 import sys
@@ -16,7 +16,7 @@ import os
 from blastMining.utility import utility
 import importlib
 
-COMMANDS = ('vote','voteSpecies','lca')
+COMMANDS = ('vote','voteSpecies','lca','besthit','full_pipeline')
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__, prog='blastMining', formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -27,8 +27,7 @@ def main():
     subparsers = parser.add_subparsers()
     for command_name in COMMANDS:
         module = importlib.import_module('blastMining.' + command_name)
-        subparser = subparsers.add_parser(command_name,
-            help=module.__doc__.split('\n')[1], description=module.__doc__)
+        subparser = subparsers.add_parser(command_name, help=module.__doc__.split('\n')[1], description=module.__doc__)
         subparser.set_defaults(func=module.main)
         module.add_arguments(subparser)
     '''
