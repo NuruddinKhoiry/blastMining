ed0edc725da4e9b008820c911d35ea51af202ea7
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-12 22:55:13 +0700

    Update __init__.py

--- blastMining/besthit/__init__.py
+++ blastMining/besthit/__init__.py
@@ -1,5 +1,10 @@
 '''
 blastMining: besthit method
+
+blastMining v.1.0.0
+
+Written by: Ahmad Nuruddin Khoiri (nuruddinkhoiri34@gmail.com)
+
 '''
 name='beshit'
 from .besthit import main, add_arguments
