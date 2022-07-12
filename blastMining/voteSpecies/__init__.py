7b8a39e47ea777f16be2923b41ec9977e86374c6
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-12 22:54:13 +0700

    Update __init__.py

--- blastMining/voteSpecies/__init__.py
+++ blastMining/voteSpecies/__init__.py
@@ -1,5 +1,10 @@
 '''
 blastMining: vote at species level for all
+
+blastMining v.1.0.0
+
+Written by: Ahmad Nuruddin Khoiri (nuruddinkhoiri34@gmail.com)
+
 '''
 name='voteSpecies'
 from .voteSpecies import main, add_arguments
