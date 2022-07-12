8d64ebf111a6b0b1bb0ade6184137254fbe568d9
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-12 22:54:19 +0700

    Update __init__.py

--- blastMining/vote/__init__.py
+++ blastMining/vote/__init__.py
@@ -1,5 +1,10 @@
 '''
 blastMining: voting method with pident cut-off
+
+blastMining v.1.0.0
+
+Written by: Ahmad Nuruddin Khoiri (nuruddinkhoiri34@gmail.com)
+
 '''
 name='vote'
 from .vote import main, add_arguments
