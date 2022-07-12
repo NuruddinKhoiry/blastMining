88485438f126456f13f6db8dca6b958d5ec57c42
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-12 22:53:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,20 @@ with open('README.md') as f:
 
 requirements = ["numpy >= 1.22.3",
 	"pandas>=1.4.2",
-	"fastnumbers>=3.2.1"]
+	"fastnumbers>=3.1.0"]
 
 setup(
     name='blastMining',
-    version = '0.1.1',
+    version = '1.0.0',
     author = 'Ahmad Nuruddin Khoiri',
     author_email = 'nuruddinkhoiri34@gmail.com',
     long_description = 'Mining NCBI BLAST outputs',
     license = 'GPLv3',
     packages=find_packages(),
     entry_points = {'console_scripts': ['blastMining = blastMining.blastMining:main']},
-    scripts=['blastMining/script/blastMining_lca.sh', 'blastMining/script/blastMining_lca2.sh'],
+    scripts=['blastMining/script/blastMining_lca.sh', 
+    'blastMining/script/blastMining_lca2.sh', 
+    'blastMining/script/tab2krona.py'],
     url='https://github.com/NuruddinKhoiry/blastMining.git',
     python_requires='>=3',
     install_requires=requirements,
