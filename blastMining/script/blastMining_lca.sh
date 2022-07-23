f098b162703305a362032d7ac0caf7d80870757b
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-23 22:40:13 +0700

    Update blastMining_lca.sh

--- blastMining/script/blastMining_lca.sh
+++ blastMining/script/blastMining_lca.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for next in $1
 do
-	echo "$(cut -f2 $next)" | taxonkit lca  -s ","
-done >> tmp_lca
+	echo "$(cut -f2 $next)" | taxonkit lca  -s "," -j $2;
+done >> ./$3/TMPDIR/tmp_lca
