37ca010cc85d1a15db0a7d064fb1b64452775057
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-23 22:40:52 +0700

    Update summary_df.py

--- blastMining/script/summary_df.py
+++ blastMining/script/summary_df.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+Copyright 2022 Ahmad Nuruddin Khoiri (nuruddinkhoiri34@gmail.com)
+
+https://github.com/NuruddinKhoiry/blastMining
+This file is a part of blastMining. blastMining is a free software: you can redistribute it and/or modify
+it under the terms of GNU General Public License v3.0. blastMining is distributed
+in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
 
 import pandas as pd
 
