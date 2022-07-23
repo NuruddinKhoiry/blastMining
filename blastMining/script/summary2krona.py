b859ffe2787954a534088329e4b4c961b477ced0
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-23 22:40:48 +0700

    Update summary2krona.py

--- blastMining/script/summary2krona.py
+++ blastMining/script/summary2krona.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+Copyright 2022 Ahmad Nuruddin Khoiri (nuruddinkhoiri34@gmail.com)
+
+https://github.com/NuruddinKhoiry/blastMining
+This file is a part of blastMining. blastMining is a free software: you can redistribute it and/or modify
+it under the terms of GNU General Public License v3.0. blastMining is distributed
+in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
 
 import pandas as pd
 
@@ -10,7 +19,7 @@ def summary2krona(table_in, table_out):
     df3 = df2.Taxa.apply(lambda x: pd.Series(str(x).split(";")))
     
     df4 = pd.concat([df2, df3], axis=1)
-    col += ['K','P','C','O','F','G','S']
+    col += ['Kingdom','Phylum','Class','Order','Family','Genus','Species']
     df4.columns = col
     
     df4.drop('Taxa',  axis=1, inplace=True)
