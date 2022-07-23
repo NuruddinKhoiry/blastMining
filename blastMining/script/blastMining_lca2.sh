1209ee5db6708977f9254a6f6799a7ea18d278c2
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-07-23 22:40:17 +0700

    Update blastMining_lca2.sh

--- blastMining/script/blastMining_lca2.sh
+++ blastMining/script/blastMining_lca2.sh
@@ -2,5 +2,5 @@
 
 for next in $1
 do
-	echo "$(cut -f2 $next)" | taxonkit lineage | taxonkit reformat -P | csvtk -H -t cut -f 1,3;
-done > tmp_lca2
+	echo "$(cut -f2 $next)" | taxonkit lineage -j $2 | taxonkit reformat -P -j $2 | csvtk -H -t cut -f 1,3 -j $2;
+done > ./$3/TMPDIR/tmp_lca2
